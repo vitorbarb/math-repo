53d14eb55a1a1a0d7a139b8d51dda23b5b674bcf
vitorbarb <79120634+vitorbarb@users.noreply.github.com>
2024-10-23 16:49:29 -0300

    Update functionIdentifier.py

--- functionIdentifier.py
+++ functionIdentifier.py
@@ -1,4 +1,4 @@
-import numpy-m 
+import numpy
 ## Main idaea is to create a program that reads both set of numbers and then tells if some item is in both setes
 # Prior to that idea the goal is to insert a function that will tell if the conjunction of the sets makes a function, ahead of that were gonna create problems to the machine to solv
 A = input("Type the A set")
@@ -8,6 +8,6 @@ my_array = numpy.array([B])
 intersection = [value for value in A if value in B]
 
 print(A , B)
-print("the intersection of the set A: %a and the set B: %a forms the intersection AUB:%a " %A %B %intersection)
+print("the intersection of the set A:", A,  "and the set B", B, "forms the intersection AUB:", intersection)
 
-
+#its horrible but it works i guess
